1a490b51e6a593f3f0c9cc6ad5674bd240d4c388
오세영 <leenight5309@naver.com>
2026-04-14 14:04:23 +0900

    feat: 0414 끝

--- js/common.js
+++ js/common.js
@@ -1,4 +1,17 @@
+/**
+ * 환율 계산?
+ * @param {*} productPrice
+ * @returns
+ */
 function taxAmount(productPrice) {
   let tax = 0.1;
   return productPrice * tax;
+}
+
+/**
+ * 한국 부자
+ * @returns
+ */
+function top5() {
+  return ["a", "b", "c", "d"];
 }
