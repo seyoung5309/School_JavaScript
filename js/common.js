33868bd3f1b4b0cf18951e6fe0a2bdaa35c47833
seyoung5309 <leenight5309@naver.co>
2026-04-20 10:32:37 +0900

    feat: 오류 수정?

--- js/common.js
+++ js/common.js
@@ -1,4 +1,15 @@
+/**
+* 부가세를 계산해서 리턴하는 함수 
+* @param {*} productPrice
+* @returns
+*/
+
 function taxAmount(productPrice) {
   let tax = 0.1;
   return productPrice * tax;
 }
+
+function getTop5() {
+	return ['a', 'b', 'c', 'd'];
+}
+
